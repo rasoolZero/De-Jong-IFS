e6cc33c22d4b93e294b4e38e1ffe0ca5750bb142
rasoolZero <rasool.kamkar20@gmail.com>
2022-09-06 19:56:17 +0430

    added setter and getter for DeJong parameters

--- include/DeJong.h
+++ include/DeJong.h
@@ -10,6 +10,22 @@ public:
 	DeJong(float a = 1.4f, float b = -2.3f, float c = 2.4f, float d = -2.1f, int const size = 512, int const iterationsPerFrame = 10000, ci::ColorA color = {0.9f,0.9f,0.9f,0.1f});
 	void update();
 	void draw();
+	float getA() { return a; }
+	float getB() { return b; }
+	float getC() { return c; }
+	float getD() { return d; }
+
+	void setA(float input) { a = input; setupFramebuffer(); }
+	void setB(float input) { b = input; setupFramebuffer(); }
+	void setC(float input) { c = input; setupFramebuffer(); }
+	void setD(float input) { d = input; setupFramebuffer(); }
+
+	ci::ColorA getColor() { return color; setupFramebuffer(); }
+	void setColor(ci::ColorA input) { color = input; }
+
+	int getIterations() { return iterationsPerFrame; }
+	void setIterations(int input) { iterationsPerFrame = input; }
+
 private:
 	vec2 point;
 	int size;
